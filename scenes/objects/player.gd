19c0f3d3e6ffc13730feff4774a85c087e746b3e
DevTime217 <headshotgamingmaster@gmail.com>
2026-04-22 14:25:21 -0400

    Player Created

---
+++ scenes/objects/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+const tile_size: Vector2 = Vector2(16, 16)
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("move_up"):
+		_move(Vector2(0, -1))
+	elif Input.is_action_pressed("move_down"):
+		_move(Vector2(0, 1))
+	elif Input.is_action_pressed("move_left"):
+		_move(Vector2(-1, 0))
+	elif Input.is_action_pressed("move_right"):
+		_move(Vector2(1, 0))
+
+func _move(dir: Vector2):
+	global_position += dir * tile_size
