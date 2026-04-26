718b2607907757bb2baa3e5037c20379a9b2f477
Adam Malik <headshotgamingmaster@gmail.com>
2026-04-26 11:58:27 -0400

    Update test_level.gd

--- scenes/levels/test_level.gd
+++ scenes/levels/test_level.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
 func _process(_delta):
-	if $Area2D.has_overlapping_bodies() == true:
+	if $KeyPickupArea.has_overlapping_bodies() == true:
 		if Input.is_action_just_pressed("interact"):
 			$PlayerUI/Inventory/Key.visible = true
